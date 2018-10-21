fd2fb1c32a0d4b820750714d9c2326214b38e0de
jywkim <jkim589@gmail.com>
2018-10-20 23:18:01 -0400

    add label displaying username

--- WindowsFormsApp/Form2.Designer.cs
+++ WindowsFormsApp/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -40,11 +41,21 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Login Successful!";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome Username";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(278, 256);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
