ac24d254f9435727f4ffdcd65c2add9ce2e701d5
jywkim <jkim589@gmail.com>
2018-10-23 23:52:05 -0400

    hide db pw

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
-        MySqlConnection conn = new MySqlConnection("server = localhost; user id = root; password = rea123; persistsecurityinfo = True; port = 3306; database = test; SslMode = none");
+        MySqlConnection conn = new MySqlConnection("server = localhost; user id = root; password = {ENTER PASSWORD HERE}; persistsecurityinfo = True; port = 3306; database = test; SslMode = none");
         int i;
         public Form1()
         {
--- WindowsFormsApp/Form2.cs
+++ WindowsFormsApp/Form2.cs
@@ -17,7 +17,6 @@ namespace WindowsFormsApp
             InitializeComponent();
 
             label1.Text = "Welcome " + text + "!";
-
         }
     }
 }
